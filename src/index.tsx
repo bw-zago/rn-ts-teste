965d136af42d6158ce137cadffbd460314a10873
Igor Zago <igor.zago@brainweb.com.br>
2020-04-03 20:01:28 -0300

    added some flowers

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,49 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, Alert, TextInput, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  Alert,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  ActivityIndicator,
+} from 'react-native';
 import Counter from './Components/Counter';
 import UserList from './Components/UserList';
 import IUser from './Interfaces/IUser';
 import {getUser} from './api/myApi';
+import LoadingState from './Components/IF';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 32,
+  },
+
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+
+  input: {
+    width: '100%',
+    marginBottom: 5,
+    borderBottomColor: '#000',
+    borderBottomWidth: 1,
+  },
+
+  button: {
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#777',
+  },
+
+  buttonTitle: {color: '#FFF'},
+});
 
 const App = () => {
   const [users, setUsers] = useState<IUser[]>([]);
@@ -13,10 +53,9 @@ const App = () => {
   const getNewUser = async () => {
     try {
       setLoading(true);
-      console.log('[newUser1]');
       const newUser = await getUser(user);
-      console.log('[newUser2]', newUser);
       setUsers([...users, newUser.data]);
+      setUser('');
     } catch (error) {
       console.log(error);
       Alert.alert('Ops!', 'Usuario não existe');
@@ -26,36 +65,23 @@ const App = () => {
   };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingHorizontal: 32,
-      }}>
-      <TextInput
-        value={user}
-        onChangeText={setUser}
-        autoCapitalize="none"
-        style={{
-          width: '100%',
-          marginBottom: 5,
-          borderBottomColor: '#000',
-          borderBottomWidth: 1,
-        }}
-      />
-      <TouchableOpacity
-        onPress={getNewUser}
-        style={{
-          width: '100%',
-          height: 50,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: '#777',
-        }}>
-        <Text style={{color: '#FFF'}}>Procurar</Text>
-      </TouchableOpacity>
-      <UserList users={users} />
+    <View style={styles.container}>
+      <LoadingState
+        loading={loading}
+        loader={<ActivityIndicator size="small" color="#000" />}>
+        <Text style={styles.title}>Perfil Github</Text>
+        <TextInput
+          value={user}
+          onChangeText={setUser}
+          autoCapitalize="none"
+          style={styles.input}
+          placeholder="Digite o perfil do usuário"
+        />
+        <TouchableOpacity onPress={getNewUser} style={styles.button}>
+          <Text style={styles.buttonTitle}>Adicionar</Text>
+        </TouchableOpacity>
+        <UserList users={users} />
+      </LoadingState>
     </View>
   );
 };
